f7a2f52f35d7a72cbec90d797c6ae21baecc56bb
tejalb <tbhamre@princeton.edu>
2014-12-22 13:07:40 -0500

    Update main.py
     Compare accuracy of my functions and kNN from sklearn

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# Compare accuracy of my functions and kNN from sklearn
+
 from functions import *
+from sklearn.neighbors import KNeighborsClassifier
+#from sklearn import neighbors
+
 
 trainingSet=[]
 testSet=[]
@@ -10,10 +15,25 @@ print 'Test set: ' + repr(len(testSet))
 # Predict
 
 predictions=[]
-k=3
+k=5
 for item in testSet:
     nbs=getNeighbors(trainingSet, item,k)
     predictions.append(getResponse(nbs))
 
 accuracy=getAccuracy(testSet,predictions)
-print 'Accuracy is ' + repr(accuracy)
+print 'Accuracy using my functions is ' + repr(accuracy)
+
+neigh=KNeighborsClassifier(n_neighbors=k)
+
+Y_train=[]
+for item in trainingSet:
+    Y_train.append(item[-1])
+    del item[-1]
+
+true_Y=[]
+for item in testSet:
+    true_Y.append(item[-1])
+    del item[-1]
+
+neigh.fit(trainingSet,Y_train)
+print 'Accuracy from sklearn is ' + repr(neigh.score(testSet,true_Y)*100)
